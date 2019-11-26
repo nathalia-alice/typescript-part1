5b7596d0e52c786679316b117531687d92490cda
nathalia-alice <nathalia.silva@compasso.com.br>
2019-11-26 17:46:26 -0300

    Guardando negociações

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -19,7 +20,7 @@ class NegociacaoController {
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value));
-
-        console.log(negociacao);
+        
+        this._negociacoes.adiciona(negociacao);
     }
 }
